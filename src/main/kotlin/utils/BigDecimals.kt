342f05b6760e3cc74aff984659297e4e50834ee4
Maciej Kaznowski <mmkaznowski@gmail.com>
2019-08-30 13:35:37 +0200

    Delete unused function Iterable<BigDecimal>.sum()

--- src/main/kotlin/utils/BigDecimals.kt
+++ src/main/kotlin/utils/BigDecimals.kt
@@ -3,14 +3,6 @@ package utils
 import java.math.BigDecimal
 import java.math.BigDecimal.ZERO
 
-fun Iterable<BigDecimal>.sum(): BigDecimal {
-    var sum = ZERO
-    forEach {
-        sum += it
-    }
-    return sum
-}
-
 fun <T> Iterable<T>.sumBy(selector: (T) -> BigDecimal): BigDecimal {
     var sum = ZERO
     forEach {
