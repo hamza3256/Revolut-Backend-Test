05764005720923c052edd7ae82bb63e093282ee9
Maciej Kaznowski <mmkaznowski@gmail.com>
2019-09-01 20:40:16 +0200

    Remove two unused classes

--- src/main/kotlin/utils/Contexts.kt
+++
@@ -1,8 +0,0 @@
-package utils
-
-import io.javalin.http.Context
-
-@Deprecated("use body")
-inline fun Context.requireParam(param: String, whenAbsent: (String) -> String): String {
-    return queryParam(param) ?: whenAbsent(param)
-}
--- src/main/kotlin/utils/Longs.kt
+++
@@ -1,5 +0,0 @@
-package utils
-
-inline fun String.toLong(whenNotLong: (String) -> Long): Long {
-    return toLongOrNull() ?: whenNotLong(this)
-}
