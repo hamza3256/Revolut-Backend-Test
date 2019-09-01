fa669e8a8674376c19923d1e5352b2b62bcce128
Maciej Kaznowski <mmkaznowski@gmail.com>
2019-09-01 13:59:34 +0200

    Add import for BigDecimal.ZERO in Money

--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -1,13 +1,14 @@
 package money
 
 import java.math.BigDecimal
+import java.math.BigDecimal.ZERO
 import java.util.*
 
 data class Money(val amount: BigDecimal, val currency: Currency) {
 
-    fun isNegative(): Boolean = amount < BigDecimal.ZERO
-    fun isPositive(): Boolean = amount > BigDecimal.ZERO
-    fun isZero(): Boolean = amount == BigDecimal.ZERO
+    fun isNegative(): Boolean = amount < ZERO
+    fun isPositive(): Boolean = amount > ZERO
+    fun isZero(): Boolean = amount == ZERO
 
     operator fun unaryMinus(): Money {
         return copy(amount = -amount)
