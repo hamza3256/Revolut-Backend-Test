190e1c7b59a7e6c1fd83ef8a1354645e1262b6a0
Maciej Kaznowski <mmkaznowski@gmail.com>
2019-09-02 13:34:22 +0200

    Add TransactionCreatorImplTest.kt

---
+++ src/test/kotlin/clients/transactions/TransactionCreatorImplTest.kt
@@ -0,0 +1,69 @@
+package clients.transactions
+
+import Clients
+import USD
+import clients.accounts.Account
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class TransactionCreatorImplTest {
+
+    private lateinit var transactionCreator: TransactionCreator
+    private lateinit var transactionRepository: TransactionRepository
+
+    @Before
+    fun beforeEachTest() {
+        transactionRepository = InMemoryTransactionRepository()
+        transactionCreator = TransactionCreatorImpl(transactionRepository)
+    }
+
+    @Test
+    fun `should insert 2 transactions into the repository`() {
+        val nikolay = Clients.nikolay(0)
+        val nikolaysAccount = Account(0, nikolay, 100.USD)
+        val vlad = Clients.vlad(1)
+        val vladsAccount = Account(1, vlad, 100.USD)
+
+        val request = TransactionCreator.Request(
+            money = 100.USD,
+            from = nikolaysAccount,
+            to = vladsAccount
+        )
+
+        transactionRepository.run {
+            assertTrue(getAll(nikolaysAccount).isEmpty())
+            assertTrue(getAll(vladsAccount).isEmpty())
+
+            transactionCreator.createTransferTransactions(request)
+
+            assertEquals(1, getAll(nikolaysAccount).size)
+            assertEquals(1, getAll(vladsAccount).size)
+        }
+    }
+
+    @Test
+    fun `should be able to resolve both transactions from one another`(){
+        val nikolay = Clients.nikolay(0)
+        val nikolaysAccount = Account(0, nikolay, 100.USD)
+        val vlad = Clients.vlad(1)
+        val vladsAccount = Account(1, vlad, 100.USD)
+
+        val request = TransactionCreator.Request(
+            money = 100.USD,
+            from = nikolaysAccount,
+            to = vladsAccount
+        )
+
+        transactionCreator.createTransferTransactions(request)
+
+        transactionRepository.run {
+            val nikolaysTransaction = getAll(nikolaysAccount).first()
+            val vladsTransaction = getAll(vladsAccount).first()
+
+            assertEquals(vladsTransaction.id, nikolaysTransaction.mirrorTransactionId)
+            assertEquals(nikolaysTransaction.id, vladsTransaction.mirrorTransactionId)
+        }
+    }
+}
